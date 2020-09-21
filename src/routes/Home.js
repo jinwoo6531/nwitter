8d5767ab85854278eeca939da53e25280769f9fe
jinwoo <jintakk6531@gmail.com>
2020-09-21 17:40:47 +0900

    9/21

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,40 @@
 import { dbService } from 'fbase';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Nweet from 'components/Nweet';
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
-  const onSubmit = async(event) => {
+  const [nweets, setNweets] = useState([]);
+
+  ///옛날 방식
+  //   const getNweets = async () => {
+  //     dbNweets = Promise로 넘어오기 때문에 async await를 꼭 써야한다.
+  //     const dbNweets = await dbService.collection('nweets').get();
+  //     dbNweets.forEach((document) => {
+  //       const nweetObject = {
+  //         ...document.data(),
+  //         id: document.id,
+  //       };
+  //       setNweets((prev) => [nweetObject, ...prev]);
+  //     });
+  //   };
+
+  useEffect(() => {
+    dbService.collection('nweets').onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection('nweets').add({
-      nweet,
+      text: nweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
     setNweet('');
   };
@@ -28,8 +55,18 @@ const Home = () => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
     </div>
   );
 };
