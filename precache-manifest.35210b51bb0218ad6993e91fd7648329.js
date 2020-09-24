91422306a1af0e85da57a41096857abc4f19cc22
jinwoo <jintakk6531@gmail.com>
2020-09-24 23:29:37 +0900

    Updates

---
+++ precache-manifest.35210b51bb0218ad6993e91fd7648329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e1c1083998fdc1edef8397afe6eb08",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "42d1f92c1e20075b5cb0",
+    "url": "/nwitter/static/css/main.51fba151.chunk.css"
+  },
+  {
+    "revision": "4cd752978452c98455c4",
+    "url": "/nwitter/static/js/2.aa07722f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.aa07722f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d1f92c1e20075b5cb0",
+    "url": "/nwitter/static/js/main.2597783f.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
